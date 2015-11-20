fada78dccbcb7eea47508529fa4019d61a5c991b
duanfeiyu1 <787880040@qq.com>
2015-11-20 09:14:53 +0800

    second modify

--- src/main/java/com/test/app/myProject/App.java
+++ src/main/java/com/test/app/myProject/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("this is my first project!");
+        System.out.println("modify git project!");
     }
 }
